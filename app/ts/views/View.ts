c2caa516acac84ed6b04d912dd668c3de86cd7f1
paulobressan <paulo.bressan@outlook.com>
2019-01-14 22:27:49 -0200

    Mudando o elemento de renderização de view para private porque somente a classe abstrata View vai utiliza-lo.

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,7 +1,7 @@
 //Classe abstrata para manipular views
 abstract class View<T> {
     //Elemento onde vai ser renderizado a mensagem
-    protected _elemento: Element;
+    private _elemento: Element;
 
     constructor(seletor: string) {
         //Buscando o elemento onde vai ser renderizado a mensagem, atravez do seu seletor
