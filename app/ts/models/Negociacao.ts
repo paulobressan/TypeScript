e5e873acf06c73e8eb6b1d3e3e9c3d3a21d24868
paulobressan <paulo.bressan@outlook.com>
2019-02-04 22:59:48 -0200

    Utilizando herança de interfaces.

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,9 +1,8 @@
 import { ClassDecorator } from '../helpers/decorators/index';
-import { Imprimivel } from './Imprimivel';
-import { Igualavel } from './Igualavel';
+import { MeuObjeto } from './MeuObjeto';
 
 @ClassDecorator("Decorator para a classe negociação")
-export class Negociacao implements Imprimivel, Igualavel<Negociacao> {
+export class Negociacao implements MeuObjeto<Negociacao> {
     //readonly vai manter a propriedade segura para escritas, somente leitura vai ser realizada externamente
     constructor(readonly data: Date, readonly quantidade: number, readonly valor: number) { }
 
